7b6c599634e2cd9bd045b0a70d99ec0ff3bb387e
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-12-10 16:50:06 +0000

    Added column types to db migrations

---
+++ DungeonsAndDragons/DungeonsAndDragons/Migrations/20181210164910_RemoveEmailColumn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DungeonsAndDragons.Migrations
+{
+    public partial class RemoveEmailColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "email",
+                table: "users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "email",
+                table: "users",
+                type: "varchar(50)",
+                nullable: true);
+        }
+    }
+}
