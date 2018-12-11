a5f4bd40877259ba47488ab2b4dea25d92c83e68
Aimée Craig <aimeecraig1@gmail.com>
2018-12-11 14:13:45 +0000

    game class created

---
+++ DungeonsAndDragons/DungeonsAndDragons/Models/Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DungeonsAndDragons.Models
+{
+    public class Game
+    {
+        public long id { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string name { get; set; }
+        public User dm_id { get; set; }
+    }
+}
