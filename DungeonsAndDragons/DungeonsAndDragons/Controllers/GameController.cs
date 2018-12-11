fd41bb5d7f013c94d94188d7634467b13c64ab8b
Aimée Craig <aimeecraig1@gmail.com>
2018-12-11 15:33:46 +0000

    game routes and views added

--- DungeonsAndDragons/DungeonsAndDragons/Controllers/GameController.cs
+++ DungeonsAndDragons/DungeonsAndDragons/Controllers/GameController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using DungeonsAndDragons.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,28 @@ namespace DungeonsAndDragons.Controllers
 {
     public class GameController : Controller
     {
-        // GET: /<controller>/
+        private readonly DungeonsAndDragonsContext _context;
+
+        public GameController(DungeonsAndDragonsContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        public IActionResult Create(string name)
+        {
+            _context.games.Add(new Game { name = name });
+            _context.SaveChanges();
+            return Redirect("Index");
         }
     }
 }
