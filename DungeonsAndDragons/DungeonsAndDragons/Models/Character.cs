84b65b6dd6b7d1ecb073275413a0d462be6f5606
melissasedgwick <melissa.sedgwick@yahoo.co.uk>
2018-12-13 15:54:05 +0000

    Added Species table

--- DungeonsAndDragons/DungeonsAndDragons/Models/Character.cs
+++ DungeonsAndDragons/DungeonsAndDragons/Models/Character.cs
@@ -9,5 +9,6 @@ namespace DungeonsAndDragons.Models
         public int id { get; set; }
         [Column(TypeName = "varchar(20)")]
         public string name { get; set; }
+        public int species_id { get; set; }
     }
 }
