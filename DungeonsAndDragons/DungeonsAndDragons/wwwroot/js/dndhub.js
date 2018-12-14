6930a0a0ad94158a71089283294752ba42bb3e5c
Aimée Craig <aimeecraig1@gmail.com>
2018-12-14 10:38:39 +0000

    game view styling fixed to match websocket view

--- DungeonsAndDragons/DungeonsAndDragons/wwwroot/js/dndhub.js
+++ DungeonsAndDragons/DungeonsAndDragons/wwwroot/js/dndhub.js
@@ -2,13 +2,15 @@
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/dndhub").build();
 
+var accepted
+
 connection.on("UpdatePlayerInvites", function (acceptedplayers, pendingplayers) {
-    var accepted = jQuery.parseJSON(acceptedplayers);
+    accepted = jQuery.parseJSON(acceptedplayers);
     var pending = jQuery.parseJSON(pendingplayers);
     $("#acceptedPlayers").empty();
     $("#pendingPlayers").empty();
     accepted.forEach(element => {
-        $("#acceptedPlayers").append("<p>" + element.userusername + "</p>");
+        $("#acceptedPlayers").append('<p>' + element.userusername + ' playing as <a href="../../PlayableCharacter/View/' + element.playablecharacterid + '">' + element.playablecharactername + '</a></p>');
     });
     pending.forEach(element => {
         $("#pendingPlayers").append("<p>" + element.userusername + "</p>");
