a2135587ec2f3cba383ffb975fb11769c589a4e5
Aimée Craig <aimeecraig1@gmail.com>
2018-12-16 22:24:12 +0000

    basic battle view created with hard coded values

--- DungeonsAndDragons/DungeonsAndDragons/Controllers/BattleController.cs
+++ DungeonsAndDragons/DungeonsAndDragons/Controllers/BattleController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using DungeonsAndDragons.Hubs;
 using DungeonsAndDragons.Models;
 using Microsoft.AspNetCore.SignalR;
+using StaticHttpContextAccessor.Helpers;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -15,11 +16,13 @@ namespace DungeonsAndDragons.Controllers
     {
         private readonly IHubContext<DnDHub> _hubcontext;
         private readonly DungeonsAndDragonsContext _context;
+        private readonly SessionHandler _sessionHandler;
 
-        public BattleController(DungeonsAndDragonsContext context, IHubContext<DnDHub> hubcontext)
+        public BattleController(DungeonsAndDragonsContext context, IHubContext<DnDHub> hubcontext, SessionHandler sessionHandler)
         {
             _context = context;
             _hubcontext = hubcontext;
+            _sessionHandler = sessionHandler;
         }
 
         public void Create(int gameid, int npc)
@@ -35,14 +38,21 @@ namespace DungeonsAndDragons.Controllers
             return Redirect($"../Game/View/{gameid}");
         }
 
-        public IActionResult View(int id, int npc)
+        public IActionResult View(int id)
         {
-            _hubcontext.Clients.Group(id.ToString()).SendAsync("StartBattleRedirect", id);
             //TODO:
             //Create new instance of Battle - view need to pass this route all required variables
             //Redirect logged out users
             //Redirect users that are not part of this game
-            @ViewBag.Battle = npc;
+
+
+            ViewBag.NPC = _context.nonplayablecharacters.SingleOrDefault(x => x.id == 1);
+            ViewBag.Players = _context.playablecharacters.Where(x => x.userid == 1);
+            ViewBag.GameDMID = 1;
+            ViewBag.UserID = _sessionHandler.GetSignedInUserID();
+
+
+            //@ViewBag.Battle = npc;
             @ViewBag.GameID = id;
             return View();
         }
